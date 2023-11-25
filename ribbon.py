40049c0bf1e7d5d57647df0a22622385a8af2496
Vitaly Samigullin <vrs@pilosus.org>
2023-11-26 00:27:47 +0100

    Improved version

--- ribbon.py
+++ ribbon.py
@@ -30,68 +30,78 @@ DEST = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
-class Plate:
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
 class Mover:
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    def __init__(self, source: list, dest: list) -> None:
-        self.source = self._transpose(source)
-        self.dest = self._transpose(dest)
-        self.dest_rows = len(self.dest)
-        self.dest_cols = len(self.dest[0])
+    def __init__(self, source: list, dest: list, overfill: bool = False) -> None:
+        self.source = source
+        self.dest = dest
+        self.moved = dest
+        self.overfill = overfill
+        self.sample_counters = defaultdict(int)
+
+    def reset(self):
+        """Reset results of moving samples"""
+        self.moved = self.dest[:]
+        self.sample_counters = defaultdict(int)
+
+    def _check_capacity(self, val: int, max_capacity: int):
+        """
+        If source plate contains more sample groups than destination
+        and flag for overflow is off, raise an exception.
+        Otherwise, when moving samples they will overfill existing groups.
+        """
+        if (val > max_capacity) and not self.overfill:
+            raise ValueError(f"Value {val} exceeds plat max capacity of {max_capacity}")
 
     @staticmethod
-    def _transpose(plate: list) -> list:
+    def _calc_dest_coords(value: int, max_capacity: int, rows: int):
         """
-        Transpose the matrix (list of lists)
+        Return column index and row index based on the value.
+        Assume values start from 1, but coordinates (col idx, row idx) from 0.
+        NB! The algorithm won't produce sparce filling in case sample values don't form
+        a continuous range, e.g. when some values are missing: 1,2,5,6,7,8,12...
         """
+        return divmod((value - 1) % max_capacity, rows)
+
+    def move(self):
+        """
+        Move samples from the source plate to destination plate.
+        Assume iteration over plates in top-to-down/left-to-right manner (column by column).
+        Assume no predefined patterns for the source plate's samples.
+        """
+        source_cols = len(self.source[0])
+        source_rows = len(self.source)
+
+        dest_cols = len(self.dest[0])
+        dest_rows = len(self.dest)
+        dest_max_capacity = dest_rows * dest_cols
+
+        for col_idx in range(source_cols):
+            for row_idx in range(source_rows):
+                sample_value = self.source[row_idx][col_idx]
+
+                self._check_capacity(val=sample_value, max_capacity=dest_max_capacity)
+                dest_row_idx, dest_col_idx = self._calc_dest_coords(value=sample_value,
+                                                                    max_capacity=dest_max_capacity,
+                                                                    rows=dest_rows)
+
+                self.sample_counters[sample_value] += 1
+                self.dest[dest_col_idx][dest_row_idx] = sample_value
+
+    def __repr__(self):
+        """
+        Return a representation string for the destination plate
+        """
+        def _fmt(s):
+            return "%-2s" % s
+
+        header = [str(i + 1) for i in range(len(self.dest[0]))]
+        rows = [header] + self.dest
         result = []
-        for col in range(len(plate[0])):
-            new_row = []
-            for row in range(len(plate)):
-                new_row.append(plate[row][col])
-            result.append(new_row)
-        return result
-
-    def source_val_to_dest_idx(self, val: int, overflow: bool = True) -> int:
-        if not overflow and val > self.dest_rows * self.dest_cols:
-            raise ValueError(f"Value {val} exceeds the plate capacity")
-
-        # divisor and remainder represent col and row respectively
-        return divmod((val - 1) % self.dest_rows, self.dest_cols)
-
-    def move(self, overflow: bool = True) -> None:
-        val_to_count = defaultdict(int)
-        filled = 0
-        max_places = self.dest_rows * self.dest_cols
-
-        for row in self.source:
-            for val in row:
-                dest_col, dest_row = divmod((val - 1) % self.dest_rows, self.dest_cols)
-                dest_current_val = self.dest[dest_col][dest_row]
-
-                print(f"Value: {val}, Dest coord {dest_col}, {dest_row}, Filled: {filled}")
-
-                self.dest[dest_col][dest_row] = val
-                if dest_current_val != val:
-                    if not overflow and filled >= max_places:
-                        raise ValueError(f"Value {val} exceeds the plate capacity")
-                    filled += 1
-                val_to_count[val] += 1
-
-    def __str__(self) -> None:
-        transposed = self._transpose(self.dest)
-        padding = [" ", " ", " "]
-        col_numbers = " ".join(padding + [str(i + 1) for i in range(len(transposed[0]))])
-        underscore = " ".join(padding + ["_" for _ in range(len(transposed[0]))])
-
-        result = [col_numbers, underscore]
-        for idx, row in enumerate(transposed):
+        for idx, row in enumerate(rows):
             letter = self.alpha[idx % len(self.alpha)]
-            new_row = [letter, " | "] + row
-            result.append(" ".join(map(str, new_row)))
+            new_row = [letter] + row
+            result.append(" ".join(map(_fmt, new_row)))
 
         return "\n".join(result)
